34883bec519c5298a75d3bf1207c04fca4a1260a
Gwenna Breton <Gwenna.breton@gu.se>
2023-06-16 13:45:55 +0200

    Create variant_counts.sh

---
+++ descriptive_analyses/variant_counts.sh
@@ -0,0 +1,5 @@
+# 2023-06-16
+# Gwenna Breton
+# Goal: count known and unreported variants in our newly generated data using an updated version of dbsnp.
+
+# Preliminary step: retrieve and prepare dbsnp file
