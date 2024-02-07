e88b1f4e3291e9cdf7095bafbdbbb5a1711ee377
Gwenna Breton <Gwenna.breton@gu.se>
2024-02-07 10:42:54 +0100

    Create prepare_scripts.sh

---
+++ descriptive-analyses/heterozygosity/prepare_scripts.sh
@@ -0,0 +1,4 @@
+#Gwenna Breton
+#2024-02-07
+#Goal: prepare script to count variant and non-variant positions in different configurations, for a given chromosome and a given set of individuals (representing a population).
+
