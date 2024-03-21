2daa73ff8beb26d48846625d9eafe5cc0e01a4ea
Gwenna Breton <Gwenna.breton@gu.se>
2024-03-21 17:48:00 +0100

    Update step3_postprocessing.sh

--- descriptive-analyses/heterozygosity/step3_postprocessing.sh
+++ descriptive-analyses/heterozygosity/step3_postprocessing.sh
@@ -10,3 +10,11 @@ for CHR in {1..22}; do
       cat header bottom > chr${CHR}_${POPNAME}_bial_count_genotypes_summary.txt
    done
 done
+
+#Make a summary of the biallelic sites for each chromosome and each population (X)
+for POPNAME in Baka Nzime BaKola Ngumba AkaMbati BaKiga BaTwa Nsua BaKonjo Karretjiepeople GuiandGana Juhoansi Nama Xun BantuHerero BantuKenya BantuTswana Biaka Dinka Esan Gambian Igbo Juhoansi_comp Khomani Lemande Luhya Luo Mandenka Maasai Mbuti Mende Mozabite Saharawi Yoruba DaiChinese Karitiana Papuan French CEU Coloured SothoSpeakers XhosaSpeakers Dinka_noHGDP Juhoansi_comp_noHGDP Mandenka_noHGDP Mbuti_noHGDP Yoruba_noHGDP French_noHGDP Papuan_noHGDP Karitiana_noHGDP DaiChinese_noHGDP; do
+  cat chrX_PARremoved_${POPNAME}_bial_count_genotypes.txt |sort |uniq -c > chrX_PARremoved_${POPNAME}_bial_count_genotypes_summary.txt;
+  tail -1 chrX_PARremoved_${POPNAME}_bial_count_genotypes_summary.txt > header ;     
+  sed \$d chrX_PARremoved_${POPNAME}_bial_count_genotypes_summary.txt > bottom;
+  cat header bottom > chrX_PARremoved_${POPNAME}_bial_count_genotypes_summary.txt;
+done
